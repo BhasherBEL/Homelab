0bb0cd575caf13cb21607aafb55c98203e6ca5c5
BhasherBEL <github.it@bhasher.com>
2023-09-26 09:53:42 +0200

    bxl-shp

---
+++ bxl-shp/deploy.sh
@@ -0,0 +1,5 @@
+cd ~/homelab/bxl-shp
+cmd=$(find . -type f \( -name "docker-compose.*.yaml" \) | awk '{printf "-f %s \0", $0}' | xargs -0 -I{} echo "docker compose {} --env-file .env up -d")
+echo $cmd
+eval $cmd
+# -name "docker-compose.yaml" -o 
